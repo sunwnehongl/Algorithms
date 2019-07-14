496b424d82237ec230dc1223c34c3fdd36c6d866
sunwnehongl <30740977+sunwnehongl@users.noreply.github.com>
2019-07-14 12:35:28 +0800

    更新说明：List 排序方法阅读后联系 提 交人：sunwenhong

--- src/test/java/com/sun/list/ArrayListTest.java
+++ src/test/java/com/sun/list/ArrayListTest.java
@@ -26,5 +26,28 @@ public class ArrayListTest {
 
         list.replaceAll(integer -> integer*integer);
         list.forEach(integer -> System.out.println(integer));
+
+        System.out.println(-10 >>> 1);
+    }
+
+    /***
+     * List 的排序算法测试
+     * List 如果长度小于7则采用的插入排序，如果长度大于等于7则采用的是归本排序
+     */
+    @Test
+    public void sortTest() {
+        List<Integer> list = new ArrayList<Integer>(){{
+            add(10);
+            add(2);
+            add(8);
+            add(4);
+            add(1);
+            add(3);
+            add(9);
+            add(11);
+        }};
+
+        list.sort((x, y) -> Integer.compare(x, y));
+        list.forEach(integer -> System.out.println(integer));
     }
 }

0c21268b20f116e7d1a2efb5b519778f4b533545
sunwnehongl <30740977+sunwnehongl@users.noreply.github.com>
2020-03-24 22:41:52 +0800

    更新说明：面试题 17.16. 按摩师 提 交人：sunwenhong

---
+++ src/main/java/com/sun/leetcode/Massage.java
@@ -0,0 +1,34 @@
+package com.sun.leetcode;
+
+import java.util.Random;
+
+/**
+ * @Auther: swh
+ * @Date: 2020/3/24 21:37
+ * @Description:
+ */
+public class Massage {
+    public static void main(String[] args) {
+        Massage massage = new Massage();
+        int[] data = {2,1};
+        System.out.println(massage.massage(data));
+    }
+
+    public int massage(int[] nums) {
+        if (nums.length == 0) {
+            return 0;
+        }
+        int[] dp = new int[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            if (i == 0) {
+                dp[i] = nums[i];
+            } else if(i == 1){
+                dp[i] =  Math.max(nums[i],dp[i-1]);
+            }else{
+                dp[i] = Math.max(dp[i-1],dp[i-2]+nums[i]);
+            }
+        }
+
+        return dp[nums.length-1];
+    }
+}
